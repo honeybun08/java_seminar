2cadc6fa200594abb34fd4990311603fec8718c0
Ulyana <ulyana_yezhgurova@mail.ru>
2023-09-03 14:37:25 +0300

    sem1 hw

---
+++ sem1_hw.java
@@ -0,0 +1,59 @@
+// автотесты!!!
+
+//public class Calculator {
+    //public int calculate(char op, int a, int b) {
+        //int resut = 0;
+        //switch (op){
+            //case '+':
+                //result = a + b;
+                //break;
+            //case '-':
+                //result = a - b;
+                //break;
+            //case '*':
+                //result = a * b;
+                //break;
+            //case '/':
+                //result = a / b;
+                //break;
+            //default:
+            //System.out.println("Неккоректынй оператор: '" + op + "'" );
+        
+        //}
+        //return result;
+      // Введите свое решение ниже
+
+
+    //}
+//}
+
+
+
+ 
+
+
+// Не удаляйте этот класс - он нужен для вывода результатов на экран и проверки
+/** 
+public class Printer{ 
+    public static void main(String[] args) { 
+        int a = 0;
+        char op = ' ';
+        int b = 0;
+
+        if (args.length == 0) {
+        // При отправке кода на Выполнение, вы можете варьировать эти параметры
+            a = 3;
+            op = '+';
+            b = 7;
+        } else {
+            a = Integer.parseInt(args[0]);
+            op = args[1].charAt(0);
+            b = Integer.parseInt(args[2]);
+        }
+
+        Calculator calculator = new Calculator();
+        int result = calculator.calculate(op, a, b);
+        System.out.println(result);
+    }
+}
+*/
